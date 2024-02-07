df6e009b7d961bca7269460cda9aa89472652b73
TANG <tangziqi1226@gmail.com>
2024-02-07 21:27:51 +0800

    feat: 134

---
+++ src/main/java/com/example/problems/canCompleteCircuit134.java
@@ -0,0 +1,19 @@
+package com.example.problems;
+
+/** */
+public class canCompleteCircuit134 {
+  public int canCompleteCircuit(int[] gas, int[] cost) {
+    int n = gas.length;
+    int sum = 0;
+    int min = 0;
+    int idx = 0;
+    for (int i = 0; i < n; i++) {
+      sum += gas[i] - cost[i];
+      if (sum < min) {
+        min = sum;
+        idx = i + 1;
+      }
+    }
+    return sum < 0 ? -1 : idx;
+  }
+}
